9748062c7fd7cccbdaedbfd51ad19bfe33aee348
bsgriggs <benjaminsgriggs@gmail.com>
2022-11-01 11:27:43 -0400

    start of media branch

---
+++ src/Spinner.editorPreview.tsx
@@ -0,0 +1,27 @@
+import { ReactElement, createElement } from "react";
+import Caption from "./components/Caption";
+import { SpinnerPreviewProps } from "../typings/SpinnerProps";
+
+export function preview({ spinnerType, spinnerCaption }: SpinnerPreviewProps): ReactElement {
+    return (
+        <div
+            style={{
+                borderRadius: "4px",
+                borderWidth: "1px",
+                borderColor: "gray",
+                borderStyle: "solid",
+                margin: "8px",
+                padding: "4px",
+                display: "flex",
+                flexDirection: "column"
+            }}
+        >
+            <span className="mx-text">{`Spinner - ${spinnerType}`}</span>
+            <Caption caption={spinnerCaption} />
+        </div>
+    );
+}
+
+export function getPreviewCss(): string {
+    return require("./ui/Spinner.css");
+}
